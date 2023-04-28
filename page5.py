796970bd639b6679cf8546c50f8a5404fcdd7668
AndreJacobsPy <andrejacobs365@gmail.com>
2023-04-27 20:05:48 -0500

    bug fix

--- page5.py
+++ page5.py
@@ -3,9 +3,8 @@ from page2 import filterer
 
 
 def user_comparisons(data):
-    # get user input
-    num = st.number_input("How many subsets?", step=1, min_value=1)
+    # first subset
+    first = filterer(data)
 
-    for i in range(num):
-        df = filterer(data)
-        st.table(df)
+    # second subset
+    second = filterer(data)
