bf0dc5278078338a70ed9739f01ee872f45c9aea
AndreJacobsPy <andrejacobs365@gmail.com>
2023-05-05 17:17:04 -0500

    add unique donors field

--- page3.py
+++ page3.py
@@ -7,13 +7,14 @@ def stats_table(df: pd.DataFrame):
     avg: float = df.amount.mean()
     sd: float = df.amount.std()
     unique_funds: int = df.fund_id.unique().size
+    unique_donors: int = df.const_id.unique().size
     small: float = df.amount.min()
     big: float = df.amount.max()
     number_donations: int = df.amount.count()
 
     # table
     stats = {
-        "Mean": [avg], "Standard Deviation": [sd], "Unique Funds": [unique_funds],
+        "Mean": [avg], "Standard Deviation": [sd], "Unique Funds": [unique_funds], "Unique Donors": [unique_donors],
         "Number of Donations": [number_donations], "Smallest Donation": [small], "Largest Donation": [big]
     }
     stats_df = pd.DataFrame(stats)
